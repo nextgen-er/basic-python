50ae812eb85e28df9be202ab7074abd490732290
Ankit Kharbanda <nextgen.erdev@gmail.com>
2026-08-26 17:03:01 +0530

    Print Fibonacci Numbers in given range

--- 01_fundamentals/02_maths_and_logics.py
+++ 01_fundamentals/02_maths_and_logics.py
@@ -15,3 +15,19 @@ if __name__ == "__main__":
         return total == original
 
     print(is_amstrong(153))
+
+    # ------------------------------------------------------------------
+    # Fibonacci Numbers
+    # ------------------------------------------------------------------
+    def fibonacci(n):
+        result = []
+
+        a, b = 0, 1
+
+        for _ in range(n):
+            result.append(a)
+            a, b = b, a + b
+
+        return result
+
+    print(fibonacci(10))
