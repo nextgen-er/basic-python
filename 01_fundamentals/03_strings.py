84a51101e484798712871546cf789f061ff41707
Ankit Kharbanda <nextgen.erdev@gmail.com>
2026-08-26 18:27:03 +0530

    To find whether the given strings are anagram or not

--- 01_fundamentals/03_strings.py
+++ 01_fundamentals/03_strings.py
@@ -14,3 +14,28 @@ if __name__ == "__main__":
         return None
 
     print(non_repeating_char("i am telling you the truth that i am"))
+
+    # ------------------------------------------------------------------
+    # Anagram without sorting
+    # ------------------------------------------------------------------
+    def is_anagram(s1, s2):
+        if len(s1) != len(s2):
+            return False
+
+        frequency = {}
+
+        for char in s1:
+            frequency[char] = frequency.get(s1, 0) + 1
+
+        for char in s2:
+            if char not in frequency:
+                return False
+
+            frequency[char] -= 1
+
+            if frequency[char] < 0:
+                return False
+
+        return all(value == 0 for value in frequency.values())
+
+    print(is_anagram("silent", "listen"))
